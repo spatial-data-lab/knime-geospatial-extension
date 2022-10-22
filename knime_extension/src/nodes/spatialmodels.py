80b3225bc32262c631f6a81485e452871812b050
Lingbo Liu <39640889+UrbanGISer@users.noreply.github.com>
2022-10-22 14:16:50 -0400

    modelling

--- knime_extension/src/nodes/spatialmodels.py
+++ knime_extension/src/nodes/spatialmodels.py
@@ -22,7 +22,7 @@ import sys
 __category = knext.category(
     path="/geo",
     level_id="spatialmodels",
-    name="Spatial Modeling",
+    name="Spatial Modelling",
     description="Spatial Models Nodes",
     # starting at the root folder of the extension_module parameter in the knime.yml file
     icon="icons/icon/SpatialStatisticsCategory.png",
