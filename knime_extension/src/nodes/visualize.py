48a33f1db305f1c73aeef347e7b822208be32494
Xiaokang Fu <1072534112@qq.com>
2022-12-03 13:53:09 -0500

    add more document for heatmap

--- knime_extension/src/nodes/visualize.py
+++ knime_extension/src/nodes/visualize.py
@@ -1110,12 +1110,14 @@ class ViewNodeKepler:
 class ViewNodeHeatmap:
     """This node will visualize the given data on a heatmap.
 
-    This node will visualize the given data as interactive heatmap. The selected weight column defines
+    This node will visualize the given data as an interactive heatmap. The selected weight column defines
     the "heat" of each data point which is visualized on a world map.
 
     The geometric elements are drawn in the order they appear in the input table. For example if you want to show
     points within a polygon you want to have the points drawn last on top of the polygon. To do so sort the input
     table to have polygons as first rows followed by the points.
+
+    Please make sure the input table does not contain any rows with missing values. 
 
     Please find more information about the heatmap [here](https://www.gislounge.com/heat-maps-in-gis/).
     """
