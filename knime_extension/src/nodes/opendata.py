spatial-data-lab/knime-geospatial-extension

commit 7724e58910886f2713b31136ec6e1165e4ff1033

Fix typo and node description of open data nodes

knime_extension/src/nodes/opendata.py
@@ -34,7 +34,7 @@ __NODE_ICON_PATH = "icons/icon/OpenDataset/"
 @knut.census_node_description(
     short_description="Retrieve geospatial data from US Census TIGER/Line",
     description="""This node retrieves the specific geospatial boundaries for one specific state of the United States.
-        The popular TIGER/Line levels are Block group, Roads, Blocks,Tracts.
+        The popular TIGER/Line levels are Block group, Roads, Blocks, Tracts.
         When the same State FIPS (2-digits) or * is used for County FIPS(3-digits), the geodata of all counties in the state will be retrieved,
         county10/20 and state10/20 can only be applicable in this case. 
         This node can help user to get the FIPS codes of target study area. Linking it to Geospatial View node will be more helpful. """,
@@ -49,13 +49,13 @@ class US2020TIGERNode:
 
     StateFips = knext.StringParameter(
         label="State FIPS (2-digits)",
-        description="The State to use [FIPS](https://transition.fcc.gov/oet/info/maps/census/fips/fips.txt)",
+        description="The State to use [FIPS.](https://transition.fcc.gov/oet/info/maps/census/fips/fips.txt)",
         default_value="",
     )
 
     County3Fips = knext.StringParameter(
         "County FIPS(3-digits)/ State FIPS or * ",
-        "The County/State FIPS code to use [FIPS](https://transition.fcc.gov/oet/info/maps/census/fips/fips.txt)",
+        "The County/State FIPS code to use [FIPS.](https://transition.fcc.gov/oet/info/maps/census/fips/fips.txt)",
         "",
     )
 
@@ -63,25 +63,25 @@ class US2020TIGERNode:
         label="TIGER/Line data type",
         description="""Available TIGER/Line are: 
         
-        - **Block:** tabblock10,tabblock20, for US Census block (the minimum level) in 2010 and 2020.
+        - **Block:** tabblock10, tabblock20, for US Census block (the minimum level) in 2010 and 2020.
         - **Block group:** bg10, bg20, for US Census block group in 2010 and 2020.
-        - **Tract:** tract10,tract20, for US Census Tract in 2010 and 2020.
-        - **County:** county10,county20, for US County in 2010 and 2020, only applicable for the setting when the input of County FIPs equals State FIPs.
-        - **State:** state10,state20, for US State in 2010 and 2020,same as County.
+        - **Tract:** tract10, tract20, for US Census Tract in 2010 and 2020.
+        - **County:** county10, county20, for US County in 2010 and 2020, only applicable for the setting when the input of County FIPs equals State FIPs.
+        - **State:** state10, state20, for US State in 2010 and 2020,same as County.
 """,
         default_value="",
         enum=[
-            "bg10",
-            "bg20",
-            "roads",
             "tabblock10",
             "tabblock20",
+            "bg10",
+            "bg20",
             "tract10",
             "tract20",
             "county10",
             "county20",
             "state10",
             "state20",
+            "roads",
         ],
     )
 
@@ -188,6 +188,7 @@ class US2020TIGERNode:
                     "OKLAHOMA",
                     "OREGON",
                     "PENNSYLVANIA",
+                    "PUERTO_RICO",
                     "RHODE_ISLAND",
                     "SOUTH_CAROLINA",
                     "SOUTH_DAKOTA",
@@ -200,7 +201,6 @@ class US2020TIGERNode:
                     "WEST_VIRGINIA",
                     "WISCONSIN",
                     "WYOMING",
-                    "PUERTO_RICO",
                 ],
             }
         )
@@ -241,53 +241,61 @@ class US2020TIGERNode:
 )
 @knut.census_node_description(
     short_description="Retrieve US 2020 Census Redistricting Data for one specific state of United States.",
-    description="""This node retrieve US 2020 Census Redistricting Data (Decennial Census P.L. 94-171 Redistricting Data).
-        This node provides all variables such as population and household information in US Census 2020 data base.
-        The default variable names are GEO_ID (geography), P1_001N(Total Population), P1_003N(Population of one race:!!White alone),
-        P1_004N(Black or African American alone), H1_001N(Total Housing Units), H1_002 (Total Occupied Housing Units).
-        Only if county is chosen for geography, then * can be input in  State FIPS (2-digits) to retrieve all the county level data of all states.
-        Before using the node, user need to sign up and get a census api key first by clicking the following hyperlink""",
+    description="""This node retrieves US 2020 Census Redistricting Data 
+(Decennial Census P.L. 94-171 Redistricting Data). This node provides all variables such as population and 
+household information from the US Census 2020 data base. The default variable names are GEO_ID (geography), 
+P1_001N (Total Population), P1_003N (Population of one race:!!White alone), P1_004N (Black or African American alone), 
+H1_001N (Total Housing Units), H1_002 (Total Occupied Housing Units). 
+
+Only if county is chosen for geography, then * can be input in State FIPS (2-digits) to retrieve all the 
+county level data of all states. 
+
+Before using the node, user need to sign up and get a Census API key first by clicking 
+[here.](https://api.census.gov/data/key_signup.html)
+    """,
     references={
         "Census API Key Sign Up": "https://api.census.gov/data/key_signup.html",
         "Decennial Census P.L. 94-171 Redistricting Data": "https://www.census.gov/programs-surveys/decennial-census/about/rdo/summary-files.html",
         "Datasets and its descendants": "https://api.census.gov/data/2020/dec.html",
         "Geography": "https://api.census.gov/data/2020/dec/pl/geography.html",
         "Variables": "https://api.census.gov/data/2020/dec/pl/variables.html",
-        "Census API examples": "https://api.census.gov/data/2020/dec/pl/examples.html",
         "FIPS code list": "https://transition.fcc.gov/oet/info/maps/census/fips/fips.txt",
+        "Census API examples": "https://api.census.gov/data/2020/dec/pl/examples.html",
     },
 )
 class USCensus2020Node:
 
     StateFips = knext.StringParameter(
         "State FIPS (2-digits)",
-        "The State [FIPS](https://transition.fcc.gov/oet/info/maps/census/fips/fips.txt) to investigate, input * for all states (while choose county for geography)",
+        "The State [FIPS](https://transition.fcc.gov/oet/info/maps/census/fips/fips.txt) to investigate, input * for all states (while choose county for geography).",
         "25",
     )
 
     County3Fips = knext.StringParameter(
         "County FIPS (3-digits)",
-        "The County [FIPS](https://transition.fcc.gov/oet/info/maps/census/fips/fips.txt) to investigate, input * for all counties",
+        "The County [FIPS](https://transition.fcc.gov/oet/info/maps/census/fips/fips.txt) to investigate, input * for all counties.",
         "017",
     )
 
     Tract6Fips = knext.StringParameter(
-        "Tract FIPS (6-digits)", "The Tract to investigate, input * for all tracts", "*"
+        "Tract FIPS (6-digits)",
+        "The Tract to investigate, input * for all tracts.",
+        "*",
     )
 
     censusapikey = knext.StringParameter(
-        "US Census APIkey", "The APIkey to use", "Input Your Census APIkey "
+        "US Census APIkey", "The Census API key to use.", "Input Your Census APIkey "
     )
 
     cols = knext.StringParameter(
         "US Census Variable Names",
-        "The US Census Variable list to use",
+        "The [US Census Variable](https://api.census.gov/data/2020/dec/pl/variables.html) list to use.",
         "GEO_ID,P1_001N,P1_003N,P1_004N,H1_001N,H1_002N",
     )
 
     geofile = knext.StringParameter(
         label="Geographic Level",
-        description=" Available geographic Level  for this node are Block, Block group, Tract and County",
+        description=" Available geographic level for this node are Block, Block group, Tract and County.",
         default_value="block group",
         enum=["block group", "block", "tract", "county"],
     )
@@ -347,13 +355,13 @@ For more details about the format and the conversion see
     description="Retrieved data from Census ACS Datasets",
 )
 @knut.census_node_description(
-    short_description="Retrieve American Community Survey 5-Year Data (2009-2020) of United States.",
-    description="""This node retrieve American Community Survey 5-Year Data (2009-2020).
+    short_description="Retrieves American Community Survey 5-Year Data (2009-2020) of United States.",
+    description="""This node retrieves American Community Survey 5-Year Data (2009-2020).
         The American Community Survey (ACS) is an ongoing survey that provides data every year -- giving 
         communities the current information they need to plan investments and services. The ACS covers a broad 
         range of topics about social, economic, demographic, and housing characteristics of the U.S. population.
-        The 5-year estimates from the ACS are period estimates that represent data collected over a period of 
-        time. The primary advantage of using multiyear estimates is the increased statistical reliability of the data 
+        The 5-year estimates from the ACS are period estimates that represent data collected over a period. 
+        The primary advantage of using multiyear estimates is the increased statistical reliability of the data 
         for less populated areas and small population subgroups. 
         The 5-year estimates are available for all geographies down to the block group level.""",
     references={
@@ -369,43 +377,52 @@ class UScensusACSNode:
 
     StateFips = knext.StringParameter(
         "State FIPS (2-digits)",
-        "The State [FIPS](https://transition.fcc.gov/oet/info/maps/census/fips/fips.txt) to investigate, input * for all states (while choose county for geography)",
+        "The State [FIPS](https://transition.fcc.gov/oet/info/maps/census/fips/fips.txt) to investigate, input * for all states (while choose county for geography).",
         "25",
     )
 
     County3Fips = knext.StringParameter(
         "County FIPS (3-digits)",
-        "The County [FIPS](https://transition.fcc.gov/oet/info/maps/census/fips/fips.txt) to investigate, input * for all counties",
+        "The County [FIPS](https://transition.fcc.gov/oet/info/maps/census/fips/fips.txt) to investigate, input * for all counties.",
         "017",
     )
 
     Tract6Fips = knext.StringParameter(
-        "Tract FIPS (6-digits)", "The Tract to investigate, input * for all tracts", "*"
+        "Tract FIPS (6-digits)",
+        "The Tract to investigate, input * for all tracts.",
+        "*",
     )
 
     censusapikey = knext.StringParameter(
-        "US Census APIkey", "The APIkey to use", "Input Your Census APIkey "
+        "US Census APIkey",
+        "The APIkey to use.",
+        "Input Your Census APIkey ",
     )
 
     cols = knext.StringParameter(
         "US Census ACS Variable Names",
-        "The US Census Variable list to use",
+        "The US Census Variable list to use.",
         "GEO_ID,B02001_001E,B02001_002E,B02001_003E",
     )
 
     geofile = knext.StringParameter(
         label="Geographic Level",
-        description=" Available geographic Level for this node are Block, Block group, Tract and County",
+        description=" Available geographic Level for this node are Block, Block group, Tract and County.",
         default_value="block group",
-        enum=["block group", "tract", "county", "state"],
+        enum=[
+            "block group",
+            "tract",
+            "county",
+            "state",
+        ],
     )
 
     year = knext.StringParameter(
-        "US Census ACS5 Year Label", "The Year label of dataset", "2020"
+        "US Census ACS5 Year Label", "The Year label of dataset.", "2020"
     )
     compatibleid = knext.BoolParameter(
         label="Make GEO_ID compatible to Tiger/Line GEOID ",
-        description=" FIPS-based GEOID for  Block, Block group, Tract and County",
+        description=" FIPS-based GEOID for Block, Block group, Tract and County.",
         default_value=lambda v: False if v <= knext.Version(1, 0, 0) else True,
         since_version="1.1.0",
     )
@@ -486,18 +503,19 @@ def get_osmnx():
 )
 @knut.osm_node_description(
     short_description="Get Points of Interests(POIs) from the Open Street Map.",
-    description="""This node downloads geospatial entities’ geometries and attributes from [OpenStreetMap](https://www.openstreetmap.org/about).
+    description="""This node downloads geospatial entities’ geometries and attributes from [OpenStreetMap.](https://www.openstreetmap.org/about)
 Results returned are the union, not intersection of each individual tag. Each result matches at least one given tag. 
-The dict keys should be OSM tags, (e.g., building, landuse, highway, etc) and the dict values should be either True 
-to retrieve all items with the given tag, or a string to get a single tag-value combination, or a list of strings to get
-multiple values for the given tag. For example, tags = {‘building’: True} would return all building footprints in the area.
-tags = {‘amenity’:True, ‘landuse’:[‘retail’,’commercial’], ‘highway’:’bus_stop’} would return all amenities, landuse=retail, 
-landuse=commercial, and highway=bus_stop.
+The place tags should be OSM tags, (e.g., building, landuse, highway, etc) and the value tags should be either True 
+to retrieve all items with the given tag, or a single value to retrieve a single tag-value combination, or a 
+comma separated list of values to get multiple values for the given tag. For example, 
+place tag=building, tag value=True would return all building footprints in the area.
+Place tag=landuse, tag value=retail, commercial would return all retail and commercial landuses.
 """,
     references={
+        "OpenStreet TagFinder": "https://tagfinder.osm.ch/",
+        "OpenStreetMap Taginfo": "https://taginfo.openstreetmap.org/",
         "OSMnx": "https://github.com/gboeing/osmnx",
         "osmnx.geometries_from_place": "https://osmnx.readthedocs.io/en/stable/osmnx.html#module-osmnx.geometries",
-        "OpenStreetMap Taginfo": "https://taginfo.openstreetmap.org/ ",
     },
 )
 class OSMdataNode:
@@ -512,14 +530,14 @@ class OSMdataNode:
     )
 
     taginfo = knext.StringParameter(
-        "Input places tags",
-        "The value for tag to specify the type of facilities",
+        "Input place tags",
+        "The value for tag to specify the type of facilities.",
         "amenity",
     )
 
     tagvalue = knext.StringParameter(
-        "Input tags value",
-        "The specific type of facilities, True for all",
+        "Input value tags",
+        "The specific type of facilities, True for all.",
         "restaurant",
     )
 
@@ -565,9 +583,10 @@ class OSMdataNode:
 )
 @knut.osm_node_description(
     short_description="Get Road Network from the Open Street Map.",
-    description="""This node downloads a geospatial network and its attributes from [OpenStreetMap.](https://www.openstreetmap.org/about)
-If the network type of drive is used for query, it will use the module add_edge_speeds to append the speed information directly. 
-The total travel time for the segment will be calculated as well with the module add_edge_travel_times. 
+    description="""This node downloads a geospatial network and its attributes from 
+    [OpenStreetMap.](https://www.openstreetmap.org/about) 
+    If the street network type 'drive' is selected, the node will append the speed information to the result table and
+    the total travel time for each segment will be calculated. 
 """,
     references={
         "OSMnx": "https://github.com/gboeing/osmnx",
@@ -590,7 +609,14 @@ class OSMnetworkNode:
         label="Street network type",
         description="Type of street network ",
         default_value="drive",
-        enum=["all_private", "all", "bike", "drive", "drive_service", "walk"],
+        enum=[
+            "all",
+            "all_private",
+            "bike",
+            "drive",
+            "drive_service",
+            "walk",
+        ],
     )
 
     def configure(self, configure_context, input_schema_1):
@@ -642,10 +668,10 @@ class OSMnetworkNode:
 )
 @knut.osm_node_description(
     short_description="Get Boundary from OpenStreetMap with Geocoding.",
-    description="""This node gets place boundary from [OpenStreetMap](https://www.openstreetmap.org/about) by the geocoding place name.
-If the query argument is a list, then which_result should be either a single value or a list with the same length as the query.
-The queries you provide must be resolvable to places in the [Nominatim database](https://nominatim.org/).
-The resulting GeoDataFrame’s geometry column contains place boundaries if they exist in OpenStreetMap.
+    description="""This node gets place boundary from [OpenStreetMap](https://www.openstreetmap.org/about) by the 
+    geocoding place name. The queries you provide must be resolvable to places in the 
+    [Nominatim database.](https://nominatim.org/) The resulting GeoDataFrame’s geometry column contains place 
+    boundaries if they exist in OpenStreetMap.
 """,
     references={
         "OSMnx": "https://github.com/gboeing/osmnx",
@@ -656,7 +682,7 @@ class OSMGeoBoundaryNode:
 
     placename = knext.StringParameter(
         label="Input place names",
-        description="Hierarchial place names delimitated with commas, such as Cambridge, MA, USA ",
+        description="Hierarchical place names delimitated with commas, such as Cambridge, MA, USA ",
         default_value="Cambridge, MA, USA",
     )
 

knime_extension/src/util/knime_utils.py
@@ -295,7 +295,7 @@ def census_node_description(short_description: str, description: str, references
         # s += "___\n\n"  # separator line between description and general part
         s += "The node is based on the data from [US Census](https://www.census.gov/) and "
         s += "[FIPS code](https://www.census.gov/library/reference/code-lists/ansi.html) "
-        s += "and uses the following related information and functions:"
+        s += "and uses the following related information and function"
         if references is not None:
             if len(references) > 1:
                 s += "s"
@@ -322,7 +322,7 @@ def osm_node_description(short_description: str, description: str, references: d
         s += f"{description}\n\n"
         # s += "___\n\n"  # separator line between description and general part
         s += "The node is based on the [OpenStreetMap project](https://www.openstreetmap.org/about) "
-        s += "and uses the following related information and functions:"
+        s += "and uses the following related information and function"
         if references is not None:
             if len(references) > 1:
                 s += "s"

commit 9cdffa5102b7fe62ba8c2ba1201473770392d92b

Fix problem with TIGER maps >=53 introduced with  commit 7724e58

knime_extension/src/nodes/opendata.py
@@ -187,7 +187,6 @@ class US2020TIGERNode:
                     "OKLAHOMA",
                     "OREGON",
                     "PENNSYLVANIA",
-                    "PUERTO_RICO",
                     "RHODE_ISLAND",
                     "SOUTH_CAROLINA",
                     "SOUTH_DAKOTA",
@@ -200,6 +199,7 @@ class US2020TIGERNode:
                     "WEST_VIRGINIA",
                     "WISCONSIN",
                     "WYOMING",
+                    "PUERTO_RICO",
                 ],
             }
         )