1d7221124f31645244efb7eb7c5967fd7ab54faa
Xiaokang Fu <1072534112@qq.com>
2023-06-19 16:27:16 -0400

    polygon support for mgwr

--- knime_extension/src/nodes/spatialmodels.py
+++ knime_extension/src/nodes/spatialmodels.py
@@ -787,8 +787,8 @@ class MultiscaleGeographicallyWeightedRegression:
         # Prepare Georgia dataset inputs
         g_y = gdf[self.dependent_variable].values.reshape((-1, 1))
         g_X = gdf[self.independent_variables].values
-        u = gdf["geometry"].x
-        v = gdf["geometry"].y
+        u = gdf["geometry"].centroid.x
+        v = gdf["geometry"].centroid.y
         g_coords = list(zip(u, v))
         # g_X = (g_X - g_X.mean(axis=0)) / g_X.std(axis=0)
         g_y = g_y.reshape((-1, 1))
