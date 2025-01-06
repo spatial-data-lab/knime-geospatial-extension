79c288be5dcf2d658ca82c15a6870fbef339b671
Tobias Koetter <tobias.koetter@knime.com>
2025-01-06 08:56:14 +0100

    Change default geo column name to geometry to be backward compatible

--- knime_extension/src/util/knime_utils.py
+++ knime_extension/src/util/knime_utils.py
@@ -53,7 +53,7 @@ __CELL_TYPE_MULTI_POLYGON = "GeoMultiPolygonCell"
 
 # The default request header to use in all nodes that perform a web request
 WEB_REQUEST_HEADER = {"User-Agent": "KNIME-Geospatial/1.1"}
-DEF_GEO_COL_NAME = "Geometry"
+DEF_GEO_COL_NAME = "geometry"
 
 
 def geo_point_col_parameter(
