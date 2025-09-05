3a0c074084a0cc4aaa56953601f896ba7566cb64
Lingbo Liu <piantu@hotmail.com>
2025-09-05 10:43:03 -0400

    move disable_scroll_zoom to base map setting
    move disable_scroll_zoom to base map setting

--- knime_extension/src/nodes/visualize.py
+++ knime_extension/src/nodes/visualize.py
@@ -357,6 +357,13 @@ class BaseMapSettings:
         ],
     )
 
+    disable_scroll_zoom = knext.BoolParameter(
+        "Disable scroll zoom",
+        "If checked, disables scroll wheel zoom on the map.",
+        default_value=False,
+        since_version="2.1.0",
+    )
+
 
 @knext.parameter_group(label="Base Map", since_version="2.1.0")
 class StaticBaseMapCxSettings:
@@ -499,13 +506,6 @@ class ViewNode:
     legend_settings = LegendSettings()
 
     label_settings = LabelSettings()
-
-    disable_scroll_zoom = knext.BoolParameter(
-        "Disable scroll zoom",
-        "If checked, disables scroll wheel zoom on the map.",
-        default_value=False,
-        since_version="2.1.0",
-    )
 
     def configure(self, configure_context, input_schema):
         self.geo_col = knut.column_exists_or_preset(
@@ -658,7 +658,7 @@ class ViewNode:
         map = gdf.explore(**kws)
 
         # Disable scroll zoom if enabled
-        if self.disable_scroll_zoom:
+        if self.basemap_setting.disable_scroll_zoom:
             # Disable scroll wheel zoom directly without showing toggle button
             map.options["scrollWheelZoom"] = False
 
